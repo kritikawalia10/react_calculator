dfee8d57ea430a9407f6a028dd46929e9701d630
Kritika Walia <141112950+kritikawalia10@users.noreply.github.com>
2023-12-08 17:22:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import Display from './components/Display'
-import Keys from './components/Keys'
 
 function App() {
   const [count, setCount] = useState(0)
